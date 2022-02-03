7e3cbeae3bf6ed36d70196baa502301b779257b8
Eric Kizaki <erickizaki@gmail.com>
2022-02-03 16:48:07 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-function App() {
+const App = () => {
+  const [rowData, setRowData] = useState([]);
+  const gridRef = useRef(null);
+
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+      .then(result => result.json())
+      .then(rowData => setRowData(rowData))
+  }, []);
+
+  const onButtonClick = e => {
+    const selectedNodes = gridRef.current.api.getSelectedNodes()
+    const selectedData = selectedNodes.map(node => node.data)
+    const selectedDataStringPresentation = selectedData.map(node => `${node.make} ${node.model}`).join(', ')
+    alert(`Selected nodes: ${selectedDataStringPresentation}`)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <button onClick={onButtonClick}>Get selected rows</button>
+      <AgGridReact
+        reactUi
+        ref={gridRef}
+        rowData={rowData}
+        rowSelection="multiple">
+        <AgGridColumn field="make" sortable={true} filter={true} checkboxSelection={true}></AgGridColumn>
+        <AgGridColumn field="model" sortable={true} filter={true}></AgGridColumn>
+        <AgGridColumn field="price" sortable={true} filter={true}></AgGridColumn>
+      </AgGridReact>
     </div>
   );
-}
+};
 
 export default App;
