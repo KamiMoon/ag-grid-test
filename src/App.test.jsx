7c134b15a0ce3d0f394f4bfeba0d2a437156791a
Eric Kizaki <erickizaki@gmail.com>
2022-02-03 19:20:53 -0600

    prevent network requests in tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('./grids/TutorialGrid', () => {
+  return function TutorialGrid() {
+    return <div>Tutorial Grid</div>;
+  };
+});
+
 test('renders label', () => {
   render(<App />);
   const appText = screen.getByText('The App');
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// https://hceris.com/jest-fail-test-if-unexpected-network-request-happens/
+
+const spies = {
+  fetch: jest.spyOn(window, 'fetch'),
+};
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+afterEach(() => {
+  expect(spies.fetch).not.toHaveBeenCalled();
+});
