e0db44156db31bbc0195941b2eb9b73ea36f2f26
Eric Kizaki <erickizaki@gmail.com>
2022-02-05 09:02:01 -0600

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import TutorialGrid from './grids/TutorialGrid';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <h2>The App</h2>
-      <TutorialGrid />
+      <nav
+        style={{
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
+        }}
+      >
+        <Link to="/tutorial-grid">Tutorial Grid</Link> | <Link to="/high-frequency-grid">High Frequency Grid</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TutorialGrid from './grids/TutorialGrid';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="tutorial-grid" element={<TutorialGrid />} />
+          {/* <Route path="high-frequency-grid" element={<Invoices />} /> */}
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>404 nothing here</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
